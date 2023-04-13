55936125bc3c4f0a5b310056006d87cb5a69dcae
giuseppevignanello <peppe.vignanello@gmail.com>
2023-04-13 17:03:07 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,88 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  data() {
+    return {
+      menuItems: [
+        {
+          title: "comics",
+          link:  "#"
+        }, 
+        {
+          title: "comics",
+          link:  "#"
+        },
+        {
+          title: "comics",
+          link:  "#"
+        },
+        {
+          title: "comics",
+          link:  "#"
+        },
+        {
+          title: "comics",
+          link:  "#"
+        },
+        {
+          title: "comics",
+          link:  "#"
+        },
+        {
+          title: "comics",
+          link:  "#"
+        },
+        {
+          title: "comics",
+          link:  "#"
+        },
+        {
+          title: "comics",
+          link:  "#"
+        },
+        {
+          title: "comics",
+          link:  "#"
+        }
+      ]
+
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header class="container d-flex justify-content-between py-3">
+    <div class="logo">
+      <img src="./assets/img/dc-logo-bg.png" alt="">
+    </div>
+    <div class="menu">
+      <ul class="row">
+        <li class="col" v-for ="item in menuItems">
+          <a :href="item.link">{{item.title }}</a>
+        </li>
+      </ul>
+    </div>
+  </header>
+  <main>
+    <div id="content" class="container">
+      <h2 class="text-white pt-4">Content goes here</h2>
+    </div>
+    <div id="band">
+      <div class="icon">
+        <img src="./assets/img/buy-comics-digital-comics.png" alt="">
+      </div>
+    </div>
+  </main>
+  <footer>
+    <div id="top_footer">
+
+    </div>
+
+    <div id="bottom_footer">
+
+    </div>
+
+  </footer>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style></style>
