69f91a2416a53d2decfe5b16382d27cb232a9022
giuseppevignanello <peppe.vignanello@gmail.com>
2023-04-13 17:36:42 +0200

    add top footer

--- src/App.vue
+++ src/App.vue
@@ -65,6 +65,53 @@ export default {
           img: "./assets/img/buy-comics-digital-comics.png",
           title: "DIGITAL COMICS"
         },
+      ],
+      footerMenuItems:[
+        {
+          title: "DC COMICS",
+          items: [
+            "Lorem",
+            "Lorem",
+            "Lorem",
+            "Lorem",
+            "Lorem",
+            "Lorem",
+            "Lorem",
+          ]
+        },
+        {
+          title: "DC COMICS",
+          items: [
+            "Lorem",
+            "Lorem",
+            "Lorem",
+            "Lorem",
+            "Lorem",
+            "Lorem",
+            "Lorem",
+            "Lorem",
+            "Lorem",
+            "Lorem",
+          ]
+        },
+        {
+          title: "DC COMICS",
+          items: [
+            "Lorem",
+            "Lorem",
+            "Lorem",
+            "Lorem",
+            "Lorem",
+          ]
+        },
+        {
+          title: "DC COMICS",
+          items: [
+            "Lorem",
+            "Lorem",
+            "Lorem",
+          ]
+        },
       ]
 
     }
@@ -73,13 +120,13 @@ export default {
 </script>
 
 <template>
-  <header class="container d-flex justify-content-between py-3">
+  <header class="container d-flex justify-content-between py-3 align-items-center">
     <div class="logo">
-      <img src="./assets/img/dc-logo-bg.png" alt="">
+      <img src="./assets/img/dc-logo.png" alt="">
     </div>
     <div class="menu">
       <ul class="row">
-        <li class="col" v-for="item in menuItems">
+        <li class="col" v-for="item in MenuItems">
           <a :href="item.link">{{ item.title }}</a>
         </li>
       </ul>
@@ -101,7 +148,20 @@ export default {
   </main>
   <footer>
     <div id="top_footer">
+      <div class="container d-flex">
+        <div class="menu d-flex">
+          <ul v-for="item in footerMenuItems">
+            <li><h5>{{ item.title }}</h5></li>
+            <li v-for="itemList in item.items">{{itemList}}</li>
+            
+        
+          </ul>
 
+        </div>
+        <div class="logo">
+          <img src="./assets/img/dc-logo-bg.png" alt="">
+        </div>
+      </div>
     </div>
 
     <div id="bottom_footer">
